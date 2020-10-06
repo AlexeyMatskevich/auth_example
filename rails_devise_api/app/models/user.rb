ebf76392a4a55f40d5cc03f88f3b8912cb37e966
Alexey Matskevich <aleksei_mackevich_job@outlook.com>
2020-10-06 14:53:05 +0300

    Add setting from https://github.com/waiting-for-dev/devise-jwt#usage

--- rails_devise_api/app/models/user.rb
+++ rails_devise_api/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::Denylist
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Denylist
 end
